40265be75a27e0f7c8c4eed07f3fdfd28b16b3b0
Ben Nesbit <benesbit@yahoo.com>
2020-09-25 09:54:27 -0700

    Create a httpClient.

--- Movies.Client/Services/CRUDService.cs
+++ Movies.Client/Services/CRUDService.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace Movies.Client.Services
 {
     public class CRUDService : IIntegrationService
     {
+        private static HttpClient _httpClient = new HttpClient();
         public async Task Run()
         {
         
