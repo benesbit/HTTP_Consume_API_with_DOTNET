0c94bcb30eb04dd209adb72aaa71521ae0eec7b6
Ben Nesbit <benesbit@yahoo.com>
2020-09-25 15:10:47 -0700

    Comment out broken code from lesson.

--- Movies.Client/Program.cs
+++ Movies.Client/Program.cs
@@ -44,11 +44,11 @@ namespace Movies.Client
         private static void ConfigureServices(IServiceCollection serviceCollection)
         {
             // add loggers           
-            serviceCollection.AddSingleton(new LoggerFactory()
-                  .AddConsole()
-                  .AddDebug());
+            //serviceCollection.AddSingleton(new LoggerFactory()
+            //    .AddConsole()
+            //    .AddDebug());
 
-            serviceCollection.AddLogging();
+            //serviceCollection.AddLogging();
 
             // register the integration service on our container with a 
             // scoped lifetime
