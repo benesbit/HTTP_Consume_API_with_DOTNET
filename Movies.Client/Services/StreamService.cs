efc96ec534413f17ae2df87f40bcfe559a0c9c0d
Ben Nesbit <benesbit@yahoo.com>
2020-09-25 15:23:52 -0700

    Write method for 'GetPosterWithStream'

--- Movies.Client/Services/StreamService.cs
+++ Movies.Client/Services/StreamService.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +11,14 @@ namespace Movies.Client.Services
     { 
         public async Task Run()
         { 
-        }          
+        }
+        
+        private async Task GetPosterWithStream()
+        {
+            var request = new HttpRequestMessage(
+                HttpMethod.Get,
+                $"api/movies/d8663e5e-7494-4f81-8739-6e0de1bea7ee/posters/{Guid.NewGuid()}");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
     }
 }
