78112ae594bc0cab9db9f2ca02c0cfd8ca82da94
Ben Nesbit <benesbit@yahoo.com>
2020-09-25 15:49:30 -0700

    Create class. Add error checking logic.

---
+++ Movies.Client/StreamExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Movies.Client
+{
+    public static class StreamExtensions
+    {
+        public static T ReadAndDeserializeFromJson<T>(this Stream stream)
+        {
+            if (stream == null)
+            {
+                throw new ArgumentNullException(nameof(stream));
+            }
+
+            if (!stream.CanRead)
+            {
+                throw new NotSupportedException("Cannot read from this stream");
+            }
+        }
+    }
+}
