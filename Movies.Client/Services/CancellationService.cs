c9c31fa7c56a8147aeaef71af0a9c50b4bf9a8b2
Ben Nesbit <benesbit@yahoo.com>
2020-09-28 11:58:17 -0700

    Add a decompression method for http client.

--- Movies.Client/Services/CancellationService.cs
+++ Movies.Client/Services/CancellationService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -7,6 +8,12 @@ namespace Movies.Client.Services
 {
     public class CancellationService : IIntegrationService
     {
+        private static HttpClient _httpClient = new HttpClient(
+            new HttpClientHandler()
+            {
+                AutomaticDecompression = System.Net.DecompressionMethods.GZip,
+            });
+
         public async Task Run()
         {
         }
