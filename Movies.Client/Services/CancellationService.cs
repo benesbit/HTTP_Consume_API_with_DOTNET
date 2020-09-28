d7a31309a085f745c7fc923b9f7639c7a27405c7
Ben Nesbit <benesbit@yahoo.com>
2020-09-28 12:00:11 -0700

    Set up http client instance.

--- Movies.Client/Services/CancellationService.cs
+++ Movies.Client/Services/CancellationService.cs
@@ -14,6 +14,14 @@ namespace Movies.Client.Services
                 AutomaticDecompression = System.Net.DecompressionMethods.GZip,
             });
 
+        public CancellationService()
+        {
+            // Set up HttpClient instance
+            _httpClient.BaseAddress = new Uri("http://localhost:57863");
+            _httpClient.Timeout = new TimeSpan(0, 0, 30);
+            _httpClient.DefaultRequestHeaders.Clear();
+        }
+
         public async Task Run()
         {
         }
