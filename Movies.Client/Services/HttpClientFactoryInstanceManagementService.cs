05bafcaa64789defe7e789f234b87326cbdddb21
Ben Nesbit <benesbit@yahoo.com>
2020-09-28 13:45:50 -0700

    Create cancellation token and use in testt method.

--- Movies.Client/Services/HttpClientFactoryInstanceManagementService.cs
+++ Movies.Client/Services/HttpClientFactoryInstanceManagementService.cs
@@ -1,14 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Movies.Client.Services
 {
     public class HttpClientFactoryInstanceManagementService : IIntegrationService
-    {  
+    {
+        private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
         public async Task Run()
         {
+            await TestDisposeHttpClient(_cancellationTokenSource.Token);
+        }
+
+        private async Task TestDisposeHttpClient(CancellationToken cancellationToken)
+        {
+            for (var i = 0; i < 10; ++i)
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var request = new HttpRequestMessage(
+                        HttpMethod.Get,
+                        "https://www.google.com");
+
+                    using (var response = await httpClient.SendAsync(request,
+                        HttpCompletionOption.ResponseHeadersRead,
+                        cancellationToken))
+                    {
+                        var stream = await response.Content.ReadAsStreamAsync();
+                        response.EnsureSuccessStatusCode();
+
+                        Console.WriteLine($"Request completed with status code <{response.StatusCode}>");
+                    }
+                }
+            }
         }
     }
 }
