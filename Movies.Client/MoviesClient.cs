131e1ff051e916c32546237fad72d6cbf2db346e
Ben Nesbit <benesbit@yahoo.com>
2020-09-28 15:17:27 -0700

    Create class.

---
+++ Movies.Client/MoviesClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Movies.Client
+{
+    public class MoviesClient
+    {
+        public HttpClient Client { get; }
+    }
+
+    public MoviesClient(HttpClient client)
+    {
+        Client = client;
+    }
+}
