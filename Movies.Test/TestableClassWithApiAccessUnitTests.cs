c1c2f3e074a85e52d739a4bdbd0598bc6d3e70b9
Ben Nesbit <benesbit@yahoo.com>
2020-09-29 09:44:13 -0700

    Make assertion

--- Movies.Test/TestableClassWithApiAccessUnitTests.cs
+++ Movies.Test/TestableClassWithApiAccessUnitTests.cs
@@ -1,14 +1,23 @@
 using System;
+using System.Net.Http;
 using Xunit;
+using Movies.Client;
+using System.Threading;
 
 namespace Movies.Test
 {
-    public class UnitTest1
+    public class TestableClassWithApiAccessUnitTests
     {
         [Fact]
-        public void Test1()
+        public void GetMovie_On401Response_MustThrowUnauthorizedApiAccessException()
         {
+            var httpClient = new HttpClient(new Return401UnauthorizedResponseHandler());
+            var testableClass = new TestableClassWithApiAccess(httpClient);
 
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            Assert.ThrowsAsync<UnauthorizedApiAccessException>(
+                () => testableClass.GetMovie(cancellationTokenSource.Token));
         }
     }
 }
